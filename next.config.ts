2e7d8205859cbe768690b6e7df058191def756d7
DSON3344 <dson3344@gmail.com>
2026-06-21 23:30:01 +0800

    config: setup static export for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // 强制导出为静态 HTML 文件
+  images: {
+    unoptimized: true, // 关闭默认的图片优化，否则在 GitHub Pages 上图片会加载失败
+  },
 };
 
 export default nextConfig;
